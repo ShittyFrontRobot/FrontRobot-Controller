4def91e028129205a598e4b7ff2cfce79af48816
berberman <1793913507@qq.com>
2019-11-02 20:28:48 +0800

    wtf??

--- TeamCode/src/main/java/org/mechdancer/common/Functions.kt
+++ TeamCode/src/main/java/org/mechdancer/common/Functions.kt
@@ -24,14 +24,11 @@ operator fun Transformation.invoke(pose: Pose2D) =
 
 private val hexArray = "0123456789ABCDEF".toCharArray()
 
-fun ByteArray.toAsciiString() = StandardCharsets.US_ASCII.decode(ByteBuffer.wrap(this))
+fun ByteArray.toAsciiString() = String(StandardCharsets.US_ASCII.decode(ByteBuffer.wrap(this)).array())
 
-fun ByteArray.toHexString(separator: CharSequence = ","): String {
+fun ByteArray.toHexString(): String {
     val sb = StringBuilder()
     for (i in indices) {
-        if (i > 0) {
-            sb.append(separator)
-        }
         val v: Int = this[i].toInt() and 255
         sb.append(hexArray[v shr 4])
         sb.append(hexArray[v and 15])
