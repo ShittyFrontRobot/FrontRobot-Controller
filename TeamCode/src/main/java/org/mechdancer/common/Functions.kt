d91edafba990492a9d3f6de1f033b501e5797048
berberman <1793913507@qq.com>
2019-11-02 22:19:54 +0800

    fuck official driver

--- TeamCode/src/main/java/org/mechdancer/common/Functions.kt
+++ TeamCode/src/main/java/org/mechdancer/common/Functions.kt
@@ -1,13 +1,14 @@
 package org.mechdancer.common
 
+import android.content.Context
+import android.hardware.usb.UsbManager
+import org.firstinspires.ftc.robotcore.internal.system.AppUtil
 import org.mechdancer.algebra.implement.vector.to2D
 import org.mechdancer.algebra.implement.vector.vector2DOfZero
 import org.mechdancer.geometry.angle.toAngle
 import org.mechdancer.geometry.angle.toRad
 import org.mechdancer.geometry.angle.toVector
 import org.mechdancer.geometry.transformation.Transformation
-import java.nio.ByteBuffer
-import java.nio.charset.StandardCharsets
 
 fun Transformation.toPose(): Pose2D {
     require(dim == 2) { "pose is a 2d transformation" }
@@ -22,22 +23,5 @@ fun Pose2D.toTransformation() =
 operator fun Transformation.invoke(pose: Pose2D) =
     Pose2D(invoke(pose.p).to2D(), invokeLinear(pose.d.toVector()).to2D().toAngle())
 
-private val hexArray = "0123456789ABCDEF".toCharArray()
 
-fun ByteArray.toAsciiString() = String(StandardCharsets.US_ASCII.decode(ByteBuffer.wrap(this)).array())
-
-fun ByteArray.toHexString(): String {
-    val sb = StringBuilder()
-    for (i in indices) {
-        val v: Int = this[i].toInt() and 255
-        sb.append(hexArray[v shr 4])
-        sb.append(hexArray[v and 15])
-    }
-    return sb.toString()
-}
-
-fun ByteArray.toDoubles() = ByteBuffer.wrap(this).let {
-    (0..(size / 8)).map { i ->
-        it.getDouble(8 * i)
-    }
-}
+fun usbManager() = AppUtil.getDefContext().getSystemService(Context.USB_SERVICE) as UsbManager
--- TeamCode/src/main/java/org/mechdancer/frontrobot/OpenMV.kt
+++ TeamCode/src/main/java/org/mechdancer/frontrobot/OpenMV.kt
@@ -1,14 +1,19 @@
 package org.mechdancer.frontrobot
 
+import android.app.PendingIntent
+import android.content.Intent
+import com.hoho.android.usbserial.driver.UsbSerialPort
+import com.hoho.android.usbserial.driver.UsbSerialProber
+import com.hoho.android.usbserial.util.SerialInputOutputManager
 import org.firstinspires.ftc.robotcore.internal.system.AppUtil
 import org.mechdancer.common.Pose3D
-import org.mechdancer.common.Uart
-import org.mechdancer.common.toDoubles
+import org.mechdancer.common.usbManager
 import org.mechdancer.ftclib.core.opmode.OpModeWithRobot
 import org.mechdancer.ftclib.core.structure.MonomericStructure
 import org.mechdancer.ftclib.core.structure.composite.Robot
-import org.mechdancer.ftclib.util.AutoCallable
 import org.mechdancer.ftclib.util.OpModeLifecycle
+import org.mechdancer.ftclib.util.SmartLogger
+import org.mechdancer.ftclib.util.warn
 import org.mechdancer.geometry.angle.toRad
 
 class OpenMV
@@ -16,29 +21,57 @@ class OpenMV
       OpModeLifecycle.Initialize<Robot>,
       OpModeLifecycle.Start,
       OpModeLifecycle.Stop,
-      AutoCallable {
+      SmartLogger,
+      SerialInputOutputManager.Listener {
 
-    private lateinit var uart: Uart
+
+    private lateinit var ioManager: SerialInputOutputManager
+    private lateinit var port: UsbSerialPort
+
+//    Use thread provided by OpMode
+//    private lateinit var executor: ExecutorService
 
     lateinit var aprilTag: Pose3D
 
     override fun init(opMode: OpModeWithRobot<Robot>) {
-        uart = Uart(AppUtil.getDefContext())
-        uart.requestPermission()
+        val drivers =
+            UsbSerialProber
+                .getDefaultProber()
+                .findAllDrivers(usbManager())
+
+        warn(drivers.joinToString { it.javaClass.simpleName })
+        val driver = drivers.first()
+        val connection = usbManager().openDevice(driver.device)
+        if (connection == null)
+            usbManager().requestPermission(driver.device, PendingIntent.getBroadcast(AppUtil.getDefContext(), 0, Intent("ch34x"), 0))
+        port = driver.ports.first()
+        port.open(connection)
+        port.setParameters(19200, 8, UsbSerialPort.STOPBITS_1, UsbSerialPort.PARITY_EVEN)
+        ioManager = SerialInputOutputManager(port, this)
     }
 
     override fun start() {
-        uart.init(Uart.Config(baudRate = Uart.BaudRate._19200))
+//        executor = Executors.newSingleThreadExecutor().apply { submit(ioManager) }
     }
 
     override fun run() {
-        uart.read(64)?.toDoubles()?.let {
-            Pose3D(it[0], it[1], it[2], it[3].toRad(), it[4].toRad(), it[5].toRad())
-        }?.let { aprilTag = it }
+        ioManager.run()
     }
 
     override fun stop() {
-        uart.close()
+        ioManager.stop()
+        port.close()
+//        executor.shutdown()
+    }
+
+    override fun onRunError(e: Exception) {
+        throw e
+    }
+
+    override fun onNewData(data: ByteArray) {
+        String(data).trim().split(",").map { it.toDouble() }.let {
+            Pose3D(it[0], it[1], it[2], it[3].toRad(), it[4].toRad(), it[5].toRad())
+        }.let { aprilTag = it }
     }
 
     override fun toString(): String = javaClass.simpleName
